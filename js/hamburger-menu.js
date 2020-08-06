b0cfd490f2bed90808785f6f089cb771baf7fc01
shrey9496 <shrey9april@gmail.com>
2020-08-07 01:09:18 +0530

    Added Section fade-in animation

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -17,3 +17,28 @@ hamburger.addEventListener('click', () => {
       }
    })
 })
+
+// Animation
+
+const sections = document.querySelectorAll('.section-fade-in')
+
+const options = {
+   root: null,
+   rootMargin: '0px',
+   threshold: 0.1
+}
+
+const callback = (entries, observer) => {
+   entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+         entry.target.style.animation = 'sectionFadeIn 1s ease forwards'
+         observer.unobserve(entry.target)
+      }
+   })
+}
+
+const observer = new IntersectionObserver(callback, options)
+
+sections.forEach((section) => {
+   observer.observe(section)
+})
