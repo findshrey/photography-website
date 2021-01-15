67442f3783108a71d0ff82c7b4c0862b89270a12
shrey9496 <shrey9april@gmail.com>
2021-01-16 00:53:09 +0530

    Refactored journal-pages and firebase query settings

--- js/firestore.js
+++ js/firestore.js
@@ -8,42 +8,20 @@ const renderImage = (doc) => {
    img.setAttribute('src', `${doc.data().sm}`)
    img.setAttribute('srcset',
       `${doc.data().sm} 500w, 
-      ${doc.data().md} 1000w, 
-      ${doc.data().lg} 1500w`
+   ${doc.data().md} 1000w, 
+   ${doc.data().lg} 1500w`
    )
    img.setAttribute('sizes', '(max-width: 800px) 90vw, 60vw')
 
    imgContainer.appendChild(img)
 }
 
-if (location.pathname === '/journal-pages/annie-rob.html') {
-   db.collection('annie-rob').get().then((snapshot) => {
-      snapshot.docs.forEach(doc => {
-         renderImage(doc)
-      })
-   })
-}
+// Get collection name
+const collectionName = location.pathname.substring(15).split(".")[0]
 
-if (location.pathname === '/journal-pages/lindsey-jayce.html') {
-   db.collection('lindsey-jayce').get().then((snapshot) => {
-      snapshot.docs.forEach(doc => {
-         renderImage(doc)
-      })
+// Get collection data
+db.collection(`${collectionName}`).get().then((snapshot) => {
+   snapshot.docs.forEach(doc => {
+      renderImage(doc)
    })
-}
-
-if (location.pathname === '/journal-pages/jessica-paul.html') {
-   db.collection('jessica-paul').get().then((snapshot) => {
-      snapshot.docs.forEach(doc => {
-         renderImage(doc)
-      })
-   })
-}
-
-if (location.pathname === '/journal-pages/desiree-bradley.html') {
-   db.collection('desiree-bradley').get().then((snapshot) => {
-      snapshot.docs.forEach(doc => {
-         renderImage(doc)
-      })
-   })
-}
+})
