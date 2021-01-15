8bc7d15387368de2c8d21c95109be0e4d494aaeb
shrey9496 <shrey9april@gmail.com>
2021-01-15 21:30:36 +0530

    Configured firestore for multiple pages

--- js/firestore.js
+++ js/firestore.js
@@ -4,24 +4,46 @@ const imgContainer = document.getElementById('js-img-container')
 const renderImage = (doc) => {
    const img = document.createElement('img')
 
-   img.setAttribute('data-id', doc.id)
+   // Setup image
    img.setAttribute('src', `${doc.data().sm}`)
-
    img.setAttribute('srcset',
       `${doc.data().sm} 500w, 
       ${doc.data().md} 1000w, 
       ${doc.data().lg} 1500w`
    )
-
    img.setAttribute('sizes', '(max-width: 800px) 90vw, 60vw')
-
-   // console.log(img);
 
    imgContainer.appendChild(img)
 }
 
-db.collection('annie-rob').get().then((snapshot) => {
-   snapshot.docs.forEach(doc => {
-      renderImage(doc)
+if (location.pathname === '/journal-pages/annie-rob.html') {
+   db.collection('annie-rob').get().then((snapshot) => {
+      snapshot.docs.forEach(doc => {
+         renderImage(doc)
+      })
    })
-})
+}
+
+if (location.pathname === '/journal-pages/lindsey-jayce.html') {
+   db.collection('lindsey-jayce').get().then((snapshot) => {
+      snapshot.docs.forEach(doc => {
+         renderImage(doc)
+      })
+   })
+}
+
+if (location.pathname === '/journal-pages/jessica-paul.html') {
+   db.collection('jessica-paul').get().then((snapshot) => {
+      snapshot.docs.forEach(doc => {
+         renderImage(doc)
+      })
+   })
+}
+
+if (location.pathname === '/journal-pages/desiree-bradley.html') {
+   db.collection('desiree-bradley').get().then((snapshot) => {
+      snapshot.docs.forEach(doc => {
+         renderImage(doc)
+      })
+   })
+}
